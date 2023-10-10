a1b97adf5d5d3820f839e9d254e40c7ee2477be3
umberto altieri <altieriumb7@gmail.com>
2023-10-10 12:34:04 +0200

    a

--- src/merge_sort_cuda.cu
+++ src/merge_sort_cuda.cu
@@ -1,24 +1,19 @@
-
 #include "../lib/merge_sort.cuh"
 
-
-void mergesort(unsigned short  *data, dim3 threadsPerBlock, dim3 blocksPerGrid,unsigned long long size) 
+void mergesort(unsigned short *data, dim3 threadsPerBlock, dim3 blocksPerGrid, unsigned long long size)
 {
     unsigned short *D_data;
     unsigned short *D_swp;
-    dim3 *D_threads;
-    dim3 *D_blocks;
-    
-    cudaMalloc((void**) &D_data, size * sizeof(long));
-    cudaMalloc((void**) &D_swp, size * sizeof(long));
+    dim3 D_threads;
+    dim3 D_blocks;
 
-    cudaMemcpy(D_data, data, size * sizeof(long), cudaMemcpyHostToDevice);
- 
-    cudaMalloc((void**) &D_threads, sizeof(dim3));
-    cudaMalloc((void**) &D_blocks, sizeof(dim3));
+    cudaMalloc((void **)&D_data, size * sizeof(unsigned short));
+    cudaMalloc((void **)&D_swp, size * sizeof(unsigned short));
 
-    cudaMemcpy(D_threads, &threadsPerBlock, sizeof(dim3), cudaMemcpyHostToDevice);
-    cudaMemcpy(D_blocks, &blocksPerGrid, sizeof(dim3), cudaMemcpyHostToDevice);
+    cudaMemcpy(D_data, data, size * sizeof(unsigned short), cudaMemcpyHostToDevice);
+
+    D_threads = threadsPerBlock;
+    D_blocks = blocksPerGrid;
 
     unsigned short *A = D_data;
     unsigned short *B = D_swp;
@@ -26,43 +21,41 @@ void mergesort(unsigned short  *data, dim3 threadsPerBlock, dim3 blocksPerGrid,u
     long nThreads = threadsPerBlock.x * threadsPerBlock.y * threadsPerBlock.z *
                     blocksPerGrid.x * blocksPerGrid.y * blocksPerGrid.z;
 
-    for (int width = 2; width < (size << 1); width <<= 1) {
-        long slices = size / ((nThreads) * width) + 1;
+    for (unsigned long long width = 2; width < (size << 1); width <<= 1)
+    {
+        unsigned long long slices = size / (nThreads * width) + 1;
 
-        
-
-        gpu_mergesort<<<blocksPerGrid, threadsPerBlock>>>(A, B, width, slices, D_threads, D_blocks,size);
+        gpu_mergesort<<<blocksPerGrid, threadsPerBlock>>>(A, B, width, slices, D_threads, D_blocks, size);
 
         A = A == D_data ? D_swp : D_data;
         B = B == D_data ? D_swp : D_data;
     }
 
-    cudaMemcpy(data, A, size * sizeof(long), cudaMemcpyDeviceToHost);
-    
-    cudaFree(A);
-    cudaFree(B);
+    cudaMemcpy(data, A, size * sizeof(unsigned short), cudaMemcpyDeviceToHost);
+
+    cudaFree(D_data);
+    cudaFree(D_swp);
 }
 
-__device__ unsigned int getIdx(dim3 *threads, dim3 *blocks) 
+__device__ unsigned int getIdx(dim3 threads, dim3 blocks)
 {
     int x;
     return threadIdx.x +
-           threadIdx.y * (x  = threads->x) +
-           threadIdx.z * (x *= threads->y) +
-           blockIdx.x  * (x *= threads->z) +
-           blockIdx.y  * (x *= blocks->z) +
-           blockIdx.z  * (x *= blocks->y);
+           threadIdx.y * (x = threads.x) +
+           threadIdx.z * (x *= threads.y) +
+           blockIdx.x * (x *= threads.z) +
+           blockIdx.y * (x *= blocks.z) +
+           blockIdx.z * (x *= blocks.y);
 }
 
-
-__global__ void gpu_mergesort(unsigned short *source, unsigned short *dest,unsigned long long width, unsigned long long slices, dim3 *threads, dim3 *blocks,unsigned long long size) 
+__global__ void gpu_mergesort(unsigned short *source, unsigned short *dest, unsigned long long width, unsigned long long slices, dim3 threads, dim3 blocks, unsigned long long size)
 {
     unsigned int idx = getIdx(threads, blocks);
-    unsigned long long start = width*idx*slices, 
-         middle, 
-         end;
+    unsigned long long start = width * idx * slices,
+                       middle,
+                       end;
 
-    for (long slice = 0; slice < slices; slice++) 
+    for (unsigned long long slice = 0; slice < slices; slice++)
     {
         if (start >= size)
             break;
@@ -74,17 +67,18 @@ __global__ void gpu_mergesort(unsigned short *source, unsigned short *dest,unsig
     }
 }
 
-__device__ void gpu_bottomUpMerge(unsigned short *source, unsigned short *dest, unsigned long long start, unsigned long long middle, unsigned long long end) 
+__device__ void gpu_bottomUpMerge(unsigned short *source, unsigned short *dest, unsigned long long start, unsigned long long middle, unsigned long long end)
 {
     unsigned long i = start;
     unsigned long j = middle;
-    for (long k = start; k < end; k++) {
-        if (i < middle && (j >= end || source[i] < source[j])) 
+    for (unsigned long long k = start; k < end; k++)
+    {
+        if (i < middle && (j >= end || source[i] < source[j]))
         {
             dest[k] = source[i];
             i++;
-        } 
-        else 
+        }
+        else
         {
             dest[k] = source[j];
             j++;
