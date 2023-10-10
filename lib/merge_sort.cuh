2bf77b8e7649a2c2e7d2c7f56fe6d0e3e7f3a801
umberto altieri <altieriumb7@gmail.com>
2023-10-10 13:05:22 +0200

    Update merge_sort.cuh

--- lib/merge_sort.cuh
+++ lib/merge_sort.cuh
@@ -8,5 +8,5 @@
 
 __device__ float min(float a, float b);
 void mergesort(float* data, dim3 threadsPerBlock, dim3 blocksPerGrid,unsigned long long size);
-__global__ void gpu_mergesort(float* source, float* dest, long width, long slices, dim3* threads, dim3* blocks.unsigned long long size) ;
+__global__ void gpu_mergesort(float* source, float* dest, long width, long slices, dim3* threads, dim3* blocks,unsigned long long size);
 __device__ void gpu_bottomUpMerge(float* source, float* dest, long start, long middle, long end);
