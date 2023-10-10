287722d8ffed3be2dd2227a4491e2d90e3b72abe
umberto altieri <altieriumb7@gmail.com>
2023-10-10 11:44:09 +0200

    Update main.cu

--- src/main.cu
+++ src/main.cu
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <math.h>
+#include <string.h> // Include for strcmp
 #include <cuda.h>
 #include <assert.h>
 #include "../lib/radix_sort.cuh"
@@ -10,49 +10,47 @@
 #include "../lib/utilsParallelSort.cuh"
 
 bool verbose;
-int main(int argc, char** argv) 
-{
-    bool write_output = false;
 
-    double start=0,end=0;
+int main(int argc, char** argv) {
+    bool write_output = false;
+    double start = 0, end = 0;
     double cput;
     unsigned short *data, *dev_data;
     unsigned long long N = 512;
-    for (int i = 1; i < argc; i++)
-    {
-        if (strcmp(argv[i], "-w") == 0)
-        {
+
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-w") == 0) {
             write_output = true;
-        }
-        else
-        {
+        } else {
             N = atoi(argv[i]);
         }
     }
+
     const size_t size_array = N * sizeof(unsigned short);
     data = (unsigned short *)malloc(size_array);
     cudaMalloc((void **)&dev_data, size_array);
     init_array(data, N);
-    sort_config = determine_config(N);
+    ParallelSortConfig sort_config = determine_config(N);
 
     sort_config.blockSize = dim3(sort_config.threads_per_block);
     sort_config.gridSize = dim3(sort_config.total_blocks);
     cudaMemcpy(dev_data, data, size_array, cudaMemcpyHostToDevice);
 
-    
     start = get_time();
 
     mergesort(data, sort_config.blockSize, sort_config.gridSize);
     end = get_time();
-    cudaPeekAtLastError());
+    cudaPeekAtLastError();
     cudaMemcpy(data, dev_data, size_array, cudaMemcpyDeviceToHost);
 
-    double cput = ((double)(end - start)) / 1000;
-    printf("\nRunning time = %d s\n", cput);
-    if(is_sorted(data,N)){
-        printf('Array sorted propertly');
-    }else{
-        printf('Array sorted improperly');
+    cput = ((double)(end - start)) / 1000;
+    printf("\nRunning time = %g s\n", cput);
+
+    if (is_sorted(data, N)) {
+        printf("Array sorted properly.\n");
+    } else {
+        printf("Array sorted improperly.\n");
     }
+
     return 0;
 }
