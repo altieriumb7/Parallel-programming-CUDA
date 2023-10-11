ff2926bd806c4e96bc0b3303fa000ad2e11445bf
umberto altieri <altieriumb7@gmail.com>
2023-10-11 05:01:31 +0200

    Update main.cu

--- src/main.cu
+++ src/main.cu
@@ -101,8 +101,9 @@ int main(int argc, char *argv[]) {
         printf("Sorted properly using Merge Sorting Parallel shared mem.");
         printf("Time for sorting: %lf\n", sorting_time[2],' ms\n');
     }else{
-        printf("Error in sorting quick sort global mem");
+        printf("Error in sorting merge sort shared mem");
     }
+    print_array(data,N)
     zero_array(data, N);
    
     //.------------------------------------------------------------------- 
