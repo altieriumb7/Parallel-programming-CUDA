3e677cdec2e199bf01acdb6a26fd0920bde82080
umberto altieri <altieriumb7@gmail.com>
2023-10-11 05:32:46 +0200

    Update merge_sort.cuh

--- lib/merge_sort.cuh
+++ lib/merge_sort.cuh
@@ -7,21 +7,13 @@
 #include <cuda_runtime.h>
 #include "../lib/constants.cuh"
 // Device function to merge two sorted arrays
-__device__ void gpu_bottomUpMerge(int* source, int* dest, int start, int middle, int end) {
-    // Function body remains the same
-}
+__device__ void gpu_bottomUpMerge(int* source, int* dest, int start, int middle, int end) ;
 
 // Device function to calculate the id of the current thread
-__device__ unsigned int getIdx(dim3* threads, dim3* blocks) {
-    // Function body remains the same
-}
+__device__ unsigned int getIdx(dim3* threads, dim3* blocks);
 
 // GPU kernel for mergesort
-__global__ void gpu_mergesort(int* source, int* dest, int size, int width, int slices, dim3* threads, dim3* blocks) {
-    // Function body remains the same
-}
+__global__ void gpu_mergesort(int* source, int* dest, int size, int width, int slices, dim3* threads, dim3* blocks);
 
 // Mergesort function
-void mergesort(int* data, int size, dim3 threadsPerBlock, dim3 blocksPerGrid) {
-    // Function body remains the same
-}
+void mergesort(int* data, int size, dim3 threadsPerBlock, dim3 blocksPerGrid);
