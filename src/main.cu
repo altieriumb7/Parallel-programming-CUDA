9c193f070c69f219a5335f2359b475fa4490c1b8
umberto altieri <altieriumb7@gmail.com>
2023-10-11 05:34:55 +0200

    Update main.cu

--- src/main.cu
+++ src/main.cu
@@ -90,7 +90,7 @@ int main(int argc, char *argv[]) {
     cudaDeviceSynchronize();
 
     t_start = time_now();
-    mergesort(data,N, config.threads_per_block, config.total_blocks);
+    mergesort(dev_data,N, config.threads_per_block, config.total_blocks);
     t_stop = time_now();
     cudaPeekAtLastError();
     cudaMemcpy(data, dev_data, size_array, cudaMemcpyDeviceToHost);
